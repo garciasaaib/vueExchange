8a9665c7d94c80d421d50d76b8a3bb485db2f70b
adranuz <garciasaaib@gmail.com>
2020-04-18 13:33:49 -0500

    Showing arrays content by  v-for

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ new Vue({
       title: 'Bitcoin',
       image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
       changePercent: 0,
+      pricesByDay: [
+        {day: 'Monday', value: 8000},
+        {day: 'Tuesday', value: 8200},
+        {day: 'Wednesday', value: 8400},
+        {day: 'Thursday', value: 8600},
+        {day: 'Friday', value: 8800},
+        {day: 'Saturday', value: 9000},
+        {day: 'Sunday', value: 10000},
+      ]
     }
   }
 })
