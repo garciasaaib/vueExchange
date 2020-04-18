d90df43b0663037088148da4431c7a7ea44822df
adranuz <garciasaaib@gmail.com>
2020-04-18 13:13:01 -0500

    Control de flujo de directivas

---
+++ app.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data () {
+    return {
+      title: 'Bitcoin',
+      image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+      changePercent: 0,
+    }
+  }
+})
