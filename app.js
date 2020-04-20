6d209f2a410452fa00f25db04c4574112209185c
adranuz <garciasaaib@gmail.com>
2020-04-20 09:24:10 -0500

    finishing the tries

--- app.js
+++ app.js
@@ -1,43 +1,42 @@
-Vue.component('CoinDetail', {
+Vue.component("CoinDetail", {
   //datos del componente padre que necesita este componente
-  props: [ 
-    'coin'
-  ],
+  props: ["coin"],
 
-  data() { //datos del componente
+  data() {
+    //datos del componente
     return {
       showPrices: false,
       value: 0,
-    }
+    };
   },
 
-  methods: { //metodos del componente
+  methods: {
+    //metodos del componente
     toggleShowPrices() {
-      this.showPrices = !this.showPrices
+      this.showPrices = !this.showPrices;
 
       //para avisar al padre que hemos cambiado este dato
-      this.$emit('change-color', 
-        this.showPrices ? 'FF96C8' : 'd3d3d3')
+      this.$emit("change-color", this.showPrices ? "FF96C8" : "d3d3d3");
     },
   },
 
   computed: {
     convertedValue() {
-      if(!this.value) {
-        return 0
+      if (!this.value) {
+        return 0;
       } else {
-        return this.value / this.coin.currentPrice
+        return this.value / this.coin.currentPrice;
       }
     },
-    title () {
-      return `${this.coin.name} ${this.coin.symbol}`
+    title() {
+      return `${this.coin.name} ${this.coin.symbol}`;
     },
   },
   created() {
-    console.log('component Created...') //sirve en apis
+    console.log("component Created..."); //sirve en apis
   },
-  mounted () {
-    console.log('component Mounted...') //dom disponible
+  mounted() {
+    console.log("component Mounted..."); //dom disponible
   },
   template: `
   <div>
@@ -72,12 +71,10 @@ Vue.component('CoinDetail', {
   </ul>
   </div>
   `,
-})
+});
 
-Vue.component('Modal', {
-  props: [
-    'title'
-  ],
+Vue.component("Modal", {
+  props: ["title"],
 
   template: `
   <div class="modal-mask">
@@ -92,59 +89,60 @@ Vue.component('Modal', {
       </div>
     </div>
   `,
-  methods: { //metodos del componente
+  methods: {
+    //metodos del componente
     closeModal() {
       //para avisar al padre que hemos cambiado este dato
-      this.$emit('close-modal')
+      this.$emit("close-modal");
     },
   },
-})
+});
 
 new Vue({
-  el: '#app',
-  data () {
+  el: "#app",
+  data() {
     return {
       btc: {
-        name: 'Bitcoin',
-        symbol: 'BTC',
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1.png',
+        name: "Bitcoin",
+        symbol: "BTC",
+        image: "https://s2.coinmarketcap.com/static/img/coins/64x64/1.png",
         changePercent: -1,
         currentPrice: 8600,
         pricesByDay: [
-          {day: 'Monday', value: 8000},
-          {day: 'Tuesday', value: 8200},
-          {day: 'Wednesday', value: 8400},
-          {day: 'Thursday', value: 8600},
-          {day: 'Friday', value: 8800},
-          {day: 'Saturday', value: 9000},
-          {day: 'Sunday', value: 10000},
+          { day: "Monday", value: 8000 },
+          { day: "Tuesday", value: 8200 },
+          { day: "Wednesday", value: 8400 },
+          { day: "Thursday", value: 8600 },
+          { day: "Friday", value: 8800 },
+          { day: "Saturday", value: 9000 },
+          { day: "Sunday", value: 10000 },
         ],
-
       },
-      color: 'a4a4a4',
+      color: "a4a4a4",
       value: 0,
       modal: false,
-      title: 'Bitcoin será el nuevo peso mexicano',
-    }
+      title: "Bitcoin será el nuevo peso mexicano",
+    };
   },
   created() {
-    console.log('Created...') //sirve en apis
+    console.log("Created..."); //sirve en apis
   },
-  mounted () {
-    console.log('Mounted...') //dom disponible
+  mounted() {
+    console.log("Mounted..."); //dom disponible
   },
-  methods:{ //eventos de la vista
+  methods: {
+    //eventos de la vista
     updateColor(color) {
-      
       //metodo para cambiar los colores
-      this.color = color || this.color
-        .split('') //crea un array con los char del string
-        .reverse() //reversa el array
-        .join('') //vuelve a concatenar el string
+      this.color =
+        color ||
+        this.color
+          .split("") //crea un array con los char del string
+          .reverse() //reversa el array
+          .join(""); //vuelve a concatenar el string
     },
     toggleModal() {
-      this.modal = !this.modal
+      this.modal = !this.modal;
     },
   },
-
-})
+});
